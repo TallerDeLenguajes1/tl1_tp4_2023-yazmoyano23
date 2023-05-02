45760092113ec922f92e3f5d61d2971f95038260
yazmoyano <moyanoyazminalejandra@gmail.com>
2023-05-01 22:52:34 -0300

    Se agrego el arreglo para las tareas realizadas

--- ejer1.c
+++ ejer1.c
@@ -10,19 +10,21 @@ struct Tarea {
 } typedef Tarea;
 
 //FUNCIONES
-
 void cargarTareas(Tarea** ListaT, int cant);
 void mostrarTareas(Tarea** ListaT, int cant);
+int controlTareas(Tarea** ListaT,Tarea** tRealizadas , int cant);
 
 //PROGRAMA PRINCIPAL
 int main()
 {
-    int cantTareas;
+    int cantTareas, cantTareasRealizadas;
 
     printf("Ingresa la cantidad de tareas: ");
     scanf("%d",&cantTareas);
+
     
     Tarea **listaTareas = (Tarea **)malloc(cantTareas * sizeof(Tarea *));
+    Tarea **TareasRealizadas = (Tarea **)malloc(cantTareas * sizeof(Tarea *)) ;
 
     //Inicialzacion del arreglo en NULL
     for (int i = 0; i < cantTareas; i++)
@@ -31,7 +33,15 @@ int main()
     }
     
     cargarTareas(listaTareas, cantTareas);
+    //mostrarTareas(listaTareas, cantTareas);
+    cantTareasRealizadas = controlTareas(listaTareas, TareasRealizadas, cantTareas);
+    printf("\n-------------TAREAS REALIZADAS---------------\n");
+    mostrarTareas(TareasRealizadas, cantTareasRealizadas);
+    printf("\n-------------LISTA MODIFICADA---------------\n");
     mostrarTareas(listaTareas, cantTareas);
+
+    //Control de tareas
+
 
     //LIBERACION DE MEMORIA (Por cada malloc tengo un free)
     for (int i = 0; i < cantTareas; i++)
@@ -45,6 +55,8 @@ int main()
 
     return 0;
 }
+
+//FUNCIONES
 
 void cargarTareas(Tarea** ListaT, int cant){
     char *buffer;
@@ -69,13 +81,41 @@ void cargarTareas(Tarea** ListaT, int cant){
 }
 
 void mostrarTareas(Tarea** ListaT, int cant){
-    printf("\n-----------TAREAS-----------");
+    for (int i = 0; i < cant; i++)
+    {
+        if (ListaT[i] != NULL) {
+            printf("\nID de la tarea: %d", ListaT[i]->TareaID);
+            printf("\nDescripci%cn de la tarea: %s",162 , ListaT[i]->Descripcion);
+            printf("\nDuraci%cn de la tarea: %d",162 , ListaT[i]->Duracion);
+            printf("\n");
+        }
+    }
+    
+}
+
+int controlTareas(Tarea** ListaT,Tarea** tRealizadas , int cant){
+    int respuesta, indice = 0;
     for (int i = 0; i < cant; i++)
     {
         printf("\nID de la tarea: %d", ListaT[i]->TareaID);
         printf("\nDescripci%cn de la tarea: %s",162 , ListaT[i]->Descripcion);
         printf("\nDuraci%cn de la tarea: %d",162 , ListaT[i]->Duracion);
         printf("\n");
+        printf("\n Se realiz%c la tarea %d (Si: 1, No: 0) \n",162 ,ListaT[i]->TareaID);
+        scanf("%d", &respuesta);
+
+        if (respuesta) {
+
+            tRealizadas[indice]->TareaID = ListaT[i]->TareaID;
+            tRealizadas[indice]->Descripcion = ListaT[i]->Descripcion;
+            tRealizadas[indice]->Duracion = ListaT[i]->Duracion; 
+            ListaT[i] = NULL;
+            indice++;
+
+        }
+        
     }
+
+    return indice;
     
 }
